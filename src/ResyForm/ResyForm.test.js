ab8d453f8144a1dcbc97b20cd17f74ad7201f7ba
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-21 13:18:30 -0400

    Flesh out reservation tests

--- src/ResyForm/ResyForm.test.js
+++ src/ResyForm/ResyForm.test.js
@@ -1,19 +1,28 @@
 import React from "react";
 import ResyForm from "./ResyForm";
 import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-const updateResys = jest.fn()
+const updateResys = jest.fn();
 
 describe("Card", () => {
-  render(
-    <ResyForm updateResys={updateResys}
-    />
-  );
-  it("should render a card", () => {
+  render(<ResyForm updateResys={updateResys} />);
+  it("should render with input fields and submit button", () => {
     expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("name")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("date")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("time")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("number")).toBeInTheDocument();
   });
-  it("Should reflect bad login", () => {
+  it("should be able to enter user info", () => {
     render(<ResyForm updateResys={updateResys} />);
-    screen.debug()
+    userEvent.type(screen.getByPlaceholderText("name"), "Blake");
+    userEvent.type(screen.getByPlaceholderText("date"), "05/23");
+    userEvent.type(screen.getByPlaceholderText("time"), "6:00");
+    userEvent.type(screen.getByPlaceholderText("number"), "3");
+    expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
+    expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
+    expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
+    expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
   });
 });

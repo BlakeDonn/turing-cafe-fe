96b5fc1736bc29c984f4754eabdafb0502f059dc
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-21 11:28:35 -0400

    Add componentDidMount to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
+import {getAllReservations} from "../api.js";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount = async () => {
+    const response = await getAllReservations();
+    console.log(response)
+  };
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form"></div>
+        <div className="resy-container"></div>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/api.js
@@ -0,0 +1,4 @@
+export const getAllReservations = async () => {
+  const response = await fetch("http://localhost:3001/api/v1/reservations");
+  return await response.json();
+};

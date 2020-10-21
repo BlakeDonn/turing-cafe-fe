345616bffef42fb6710a1f0a190cd5d5e7489a89
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-21 13:28:45 -0400

    Add settup for app integration test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("Should be able to add new reservation", () => {
+  render(<App />);
+  userEvent.type(screen.getByPlaceholderText("name"), "Blake");
+  userEvent.type(screen.getByPlaceholderText("date"), "05/23");
+  userEvent.type(screen.getByPlaceholderText("time"), "6:00");
+  userEvent.type(screen.getByPlaceholderText("number"), "3");
+  screen.debug();
 });

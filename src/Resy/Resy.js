55f48472c5e1d8e153b815d1fdd1576791af7ece
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-21 11:53:30 -0400

    Display resys to DOM

--- src/Resy/Resy.js
+++ src/Resy/Resy.js
@@ -1,5 +1,12 @@
 import React from "react";
 export const Resy = (props) => {
-  console.log(props)
-  return <></>;
+  return (
+    <div key={props.id}>
+      <h3>{props.name}</h3>
+      <p>{props.date}</p>
+      <p>{props.time}</p>
+      <p>{props.number}</p>
+      <button>Cancel</button>
+    </div>
+  );
 };

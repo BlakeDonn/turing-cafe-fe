02473319584a4826e4620b05ca990f6451d6e80a
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-21 13:01:07 -0400

    Add initial form test

--- src/ResyForm/ResyForm.test.js
+++ src/ResyForm/ResyForm.test.js
@@ -1,23 +1,15 @@
-import React from 'react'
-import ResyForm from './ResyForm'
+import React from "react";
+import ResyForm from "./ResyForm";
 import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
+const updateResys = jest.fn()
 
 describe("Card", () => {
   render(
-    <ResyForm
-      average_rating={9}
-      backdrop_path="testback"
-      id={694919}
-      poster_path="testpath"
-      release_date="test-date"
-      title="Test Title"
-      favoriteMovies={[1234]}
+    <ResyForm updateResys={updateResys}
     />
   );
   it("should render a card", () => {
-    expect(screen.getByText("Test Title")).toBeInTheDocument();
-    expect(screen.getByAltText("poster of Test Title")).toBeInTheDocument();
-    expect(screen.getByText("Average Rating: 9")).toBeInTheDocument();
+    expect(screen.getByText("Make a Reservation")).toBeInTheDocument();
   });
 });
